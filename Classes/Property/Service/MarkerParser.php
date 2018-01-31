09ac61765cf225164e28267ced8dfada3e929f57
Nathan Boiron <nathan.boiron@gmail.com>
2018-01-31 13:59:19 +0100

    [FEATURE] Change the marker syntax from `#FOO#` to `{foo}`
    Also adds support for dotted path syntax, meaning sub-values can be
    accessed. For instance `{foo.bar}` will return the value of the `bar`
    property of the `foo` object/array.

--- Classes/Property/Service/MarkerParser.php
+++ Classes/Property/Service/MarkerParser.php
@@ -17,6 +17,7 @@
 namespace CuyZ\Notiz\Property\Service;
 
 use CuyZ\Notiz\Domain\Property\Marker;
+use TYPO3\CMS\Extbase\Reflection\ObjectAccess;
 
 /**
  * Helper class to parse markers and replace them in a given string.
@@ -30,12 +31,87 @@ class MarkerParser
      */
     public function replaceMarkers($string, array $markers)
     {
+        if (empty($markers)) {
+            return $string;
+        }
+
+        $markers = $this->keyMarkersByName($markers);
+
+        list($identifiers, $variables, $roots) = $this->matchMarkers($string);
+
+        if (empty($identifiers)) {
+            return $string;
+        }
+
         $replacePairs = [];
 
-        foreach ($markers as $marker) {
-            $replacePairs[$marker->getFormattedName()] = $marker->getValue();
+        foreach ($variables as $index => $variable) {
+            $identifier = $identifiers[$index];
+            $root = $roots[$index];
+            $marker = $markers[$root];
+
+            $value = $this->getVariableValue($variable, $root, $marker);
+
+            $replacePairs[$identifier] = $value;
         }
 
         return strtr($string, $replacePairs);
+    }
+
+    /**
+     * This will set each marker's name as the array key
+     *
+     * @param array $markers
+     * @return array
+     */
+    private function keyMarkersByName(array $markers)
+    {
+        $aux = [];
+
+        // This will avoid looping on markers for each variable
+        foreach ($markers as $marker) {
+            $aux[$marker->getName()] = $marker;
+        }
+
+        return $aux;
+    }
+
+    /**
+     * This method will find all markers in the string
+     *
+     * @param $string
+     * @return array
+     */
+    private function matchMarkers($string)
+    {
+        preg_match_all(
+            '/{
+                (
+                    ([a-z]+[a-z0-1]*)           # The root variable
+                    (?:\.[a-z]+[a-z0-1]*)*      # The other parts
+                )
+            }/xi',
+            $string,
+            $matches
+        );
+
+        return $matches;
+    }
+
+    /**
+     * @param string $variable
+     * @param string $root
+     * @param Marker $marker
+     * @return mixed
+     */
+    protected function getVariableValue($variable, $root, Marker $marker)
+    {
+        // We need to have the root name to allow the ObjectAccess class to
+        // retrieve the value.
+        $target = [
+            $root => $marker->getValue(),
+        ];
+
+        return ObjectAccess::getPropertyPath($target, $variable);
     }
 }
