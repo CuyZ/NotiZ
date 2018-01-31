ff503fb54f19019fc14190981cab4523f6db9698
Nathan Boiron <nathan.boiron@gmail.com>
2018-01-31 12:01:08 +0100

    [TASK] Fix the identation for the last time!

--- Classes/Property/Service/MarkerParser.php
+++ Classes/Property/Service/MarkerParser.php
@@ -86,11 +86,11 @@ class MarkerParser
     {
         preg_match_all(
             '/{
-                        (
-                            ([a-z]+[a-z0-1]*)           # The root variable
-                            (?:\.[a-z]+[a-z0-1]*)*      # The other parts
-                        )
-                    }/xi',
+                (
+                    ([a-z]+[a-z0-1]*)           # The root variable
+                    (?:\.[a-z]+[a-z0-1]*)*      # The other parts
+                )
+            }/xi',
             $string,
             $matches
         );
