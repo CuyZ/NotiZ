e07d886cf4b9e0785cbd666f47396f3ec05494de
Romain Canon <romain.hydrocanon@gmail.com>
2018-02-08 15:32:38 +0100

    [FEATURE] Enhance `slot.render` view-helper
    This view-helper can now use two new features, for a total of three ways
    to render a slot.
    
    Inline
    ------
    
    The processed slot value will be returned.
    
    ```html
    <nz:slot.render name="MySlot"
                    markers="{foo: 'bar'}" />
    ```
    
    Conditional
    -----------
    
    Can be used to check whether the slot exists, and do something if it
    doesn't. When using this way, a variable `slotValue` becomes accessible
    within the view-helper, that contains the processed value of the slot.
    However, this variable is filled only when the slot exists and can be
    processed.
    
    ```html
    <nz:slot.render name="SomeOptionalSlot">
        <f:then>
            {slotValue -> f:format.html()}
        </f:then>
        <f:else>
            Some default value
        </f:else>
    </nz:slot.render>
    ```
    
    Wrapping
    --------
    
    You may need to add HTML around the slot value only when the slot
    exists.
    
    ```html
    <nz:slot.render name="SomeOptionalSlot">
        <hr />
        <div class="some-class">
            {slotValue}
        </div>
    </nz:slot.render>
    ```

--- Classes/ViewHelpers/Slot/RenderViewHelper.php
+++ Classes/ViewHelpers/Slot/RenderViewHelper.php
@@ -16,39 +16,102 @@
 
 namespace CuyZ\Notiz\ViewHelpers\Slot;
 
+use Closure;
 use CuyZ\Notiz\Core\Exception\DuplicateEntryException;
-use CuyZ\Notiz\Core\Exception\EntryNotFoundException;
 use CuyZ\Notiz\Core\Property\Service\MarkerParser;
 use CuyZ\Notiz\Domain\Property\Marker;
+use CuyZ\Notiz\Service\Container;
 use CuyZ\Notiz\View\Slot\SlotContainer;
-use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+use CuyZ\Notiz\View\Slot\SlotView;
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
+use TYPO3\CMS\Fluid\Core\Compiler\TemplateCompiler;
+use TYPO3\CMS\Fluid\Core\Parser\SyntaxTree\AbstractNode;
+use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractConditionViewHelper;
+use TYPO3\CMS\Fluid\Core\ViewHelper\ViewHelperVariableContainer;
 
-class RenderViewHelper extends AbstractViewHelper
+/**
+ * Will process and render the wanted slot, by getting the value filled by the
+ * user and replacing markers within it.
+ *
+ * This view-helper can be used in several ways:
+ *
+ * Inline
+ * ------
+ *
+ * The processed slot value will be returned.
+ *
+ * ```
+ * <nz:slot.render name="MySlot"
+ *                 markers="{foo: 'bar'}" />
+ * ```
+ *
+ * Conditional
+ * -----------
+ *
+ * Can be used to check whether the slot exists, and do something if it doesn't.
+ *
+ * When using this way, a variable `slotValue` becomes accessible within the
+ * view-helper, that contains the processed value of the slot. However, this
+ * variable is filled only when the slot exists and can be processed.
+ *
+ * ```
+ * <nz:slot.render name="SomeOptionalSlot">
+ *     <f:then>
+ *         {slotValue -> f:format.html()}
+ *     </f:then>
+ *     <f:else>
+ *         Some default value
+ *     </f:else>
+ * </nz:slot.render>
+ * ```
+ *
+ * Wrapping
+ * --------
+ *
+ * You may need to add HTML around the slot value only when the slot exists.
+ *
+ * ```
+ * <nz:slot.render name="SomeOptionalSlot">
+ *     <hr />
+ *
+ *     <div class="some-class">
+ *         {slotValue}
+ *     </div>
+ * </nz:slot.render>
+ * ```
+ */
+class RenderViewHelper extends AbstractConditionViewHelper
 {
-    const SLOT_CONTAINER = 'SlotContainer';
-    const SLOT_VALUES = 'SlotValues';
-    const MARKERS = 'Marker';
+    /**
+     * Unfortunately, the rendering context is not passed to the method
+     * `evaluateCondition`. We need to first save the variable container in the
+     * class before the method is called.
+     *
+     * @var ViewHelperVariableContainer
+     */
+    protected static $currentVariableContainer;
 
     /**
-     * @var MarkerParser
+     * @deprecated Must be removed when TYPO3 v7 is not supported anymore.
+     *
+     * @var AbstractNode[]
      */
-    protected $markerParser;
-
-    /**
-     * @param MarkerParser $markerParser
-     */
-    public function __construct(MarkerParser $markerParser)
-    {
-        $this->markerParser = $markerParser;
-    }
+    private $childNodesLegacy = [];
 
     /**
      * @inheritdoc
      */
     public function initializeArguments()
     {
+        parent::initializeArguments();
+
         $this->registerArgument('name', 'string', 'Name of the slot that will be rendered.', true);
         $this->registerArgument('markers', 'array', 'Additional markers that will be added to the slot and can be used within the FlexForm.', false, []);
+
+        if (version_compare(VersionNumberUtility::getCurrentTypo3Version(), '8.0.0', '>=')) {
+            unset($this->argumentDefinitions['condition']);
+        }
     }
 
     /**
@@ -56,17 +119,74 @@ class RenderViewHelper extends AbstractViewHelper
      */
     public function render()
     {
-        $result = '';
-        $name = $this->arguments['name'];
-        $newMarkers = $this->arguments['markers'];
+        if (empty($this->childNodesLegacy)) {
+            return self::getSlotValue($this->arguments, $this->renderingContext);
+        } else {
+            self::addSlotValueToVariables($this->arguments, $this->renderingContext);
 
-        $slotContainer = $this->getSlotContainer();
-        $slotValues = $this->getSlotValues();
-        $markers = $this->getMarkers();
-
-        if (!$slotContainer->has($name)) {
-            throw EntryNotFoundException::slotNotFound($name);
+            return parent::render();
         }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function renderStatic(array $arguments, Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
+    {
+        self::addSlotValueToVariables($arguments, $renderingContext);
+
+        return parent::renderStatic($arguments, $renderChildrenClosure, $renderingContext);
+    }
+
+    /**
+     * Adds a new variable `slotValue` to the view, that contains the processed
+     * value of the slot.
+     *
+     * @param array $arguments
+     * @param RenderingContextInterface $renderingContext
+     */
+    protected static function addSlotValueToVariables(array $arguments, RenderingContextInterface $renderingContext)
+    {
+        $slotValue = self::getSlotValue($arguments, $renderingContext);
+        $renderingContext->getTemplateVariableContainer()->add('slotValue', $slotValue);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function compile($argumentsName, $closureName, &$initializationPhpCode, AbstractNode $node, TemplateCompiler $compiler)
+    {
+        if (empty($node->getChildNodes())) {
+            return sprintf(
+                '%s::getSlotValue(%s, $renderingContext)',
+                get_class($this),
+                $argumentsName
+            );
+        } else {
+            return parent::compile($argumentsName, $closureName, $initializationPhpCode, $node, $compiler);
+        }
+    }
+
+    /**
+     * Fetches the final value of the wanted slot, by getting the user value and
+     * replacing markers in it.
+     *
+     * @param array $arguments
+     * @param RenderingContextInterface $renderingContext
+     * @return string
+     *
+     * @throws DuplicateEntryException
+     */
+    public static function getSlotValue(array $arguments, RenderingContextInterface $renderingContext)
+    {
+        self::$currentVariableContainer = $renderingContext->getViewHelperVariableContainer();
+
+        $result = '';
+        $name = $arguments['name'];
+        $newMarkers = $arguments['markers'];
+
+        $slotValues = self::getSlotValues();
+        $markers = self::getMarkers();
 
         foreach ($newMarkers as $key => $value) {
             if (isset($markers[$key])) {
@@ -82,37 +202,59 @@ class RenderViewHelper extends AbstractViewHelper
                 $markers[$key] = $marker;
             }
 
-            $result = $this->markerParser->replaceMarkers(
+            $markerParser = Container::get(MarkerParser::class);
+
+            $result = $markerParser->replaceMarkers(
                 $slotValues[$name],
                 $markers
             );
         }
 
-
         return $result;
+    }
+
+    /**
+     * @param array $arguments
+     * @return bool
+     */
+    protected static function evaluateCondition($arguments = null)
+    {
+        return self::getSlotContainer()->has($arguments['name']);
+    }
+
+    /**
+     * @deprecated Must be removed when TYPO3 v7 is not supported anymore.
+     *
+     * @param array $childNodes
+     */
+    public function setChildNodes(array $childNodes)
+    {
+        $this->childNodesLegacy = $childNodes;
+
+        parent::setChildNodes($childNodes);
     }
 
     /**
      * @return SlotContainer
      */
-    protected function getSlotContainer()
+    protected static function getSlotContainer()
     {
-        return $this->viewHelperVariableContainer->get(__CLASS__, self::SLOT_CONTAINER);
+        return self::$currentVariableContainer->get(SlotView::class, SlotView::SLOT_CONTAINER);
     }
 
     /**
      * @return array
      */
-    protected function getSlotValues()
+    protected static function getSlotValues()
     {
-        return $this->viewHelperVariableContainer->get(__CLASS__, self::SLOT_VALUES);
+        return self::$currentVariableContainer->get(SlotView::class, SlotView::SLOT_VALUES);
     }
 
     /**
      * @return array
      */
-    protected function getMarkers()
+    protected static function getMarkers()
     {
-        return $this->viewHelperVariableContainer->get(__CLASS__, self::MARKERS);
+        return self::$currentVariableContainer->get(SlotView::class, SlotView::MARKERS);
     }
 }
