4595aae7ca8a5493c97e72b3d352214db14df673
Romain Canon <romain.hydrocanon@gmail.com>
2018-02-13 14:06:21 +0100

    [FEATURE] Allow RTE mode in text slot (#46)
    Text slots can now use RTE mode like this:
    
    ```html
    <nz:slot.text name="MySlot"
                  label="My slot"
                  rte="true"
                  rteMode="my-ckeditor-preset" />
    ```
    
    You can use your own CKEditor preset by filling the argument `rteMode`.
    
    Don't forget to wrap the rendering of your slot like this:
    
    ```html
    <f:format.html>
        <nz:slot.render name="MySlot" />
    </f:format.html>
    ```
    
    A legacy mode is also introduced, allowing old configuration from
    EXT:rtehtmlarea to work as well in the `rteMode` argument.
    
    Closes #24

--- Classes/View/Slot/Application/TextSlot.php
+++ Classes/View/Slot/Application/TextSlot.php
@@ -16,17 +16,100 @@
 
 namespace CuyZ\Notiz\View\Slot\Application;
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 class TextSlot extends Slot
 {
+    /**
+     * @var bool
+     */
+    protected $rte;
+
+    /**
+     * The RTE mode that can be used for:
+     *
+     * - CKEditor: must contain a valid preset that was registered in the global
+     *             configuration of the extension.
+     * - RTEHtmlArea: extra configuration for the RTE (field `defaultExtras`).
+     *
+     * @var string
+     */
+    protected $rteMode;
+
+    /**
+     * @param string $name
+     * @param string $label
+     * @param bool $rte
+     * @param string $rteMode
+     */
+    public function __construct($name, $label, $rte, $rteMode)
+    {
+        parent::__construct($name, $label);
+
+        $this->rte = $rte;
+        $this->rteMode = $rteMode;
+    }
+
     /**
      * @return string
      */
     public function getFlexFormConfiguration()
     {
-        return <<<XML
-    <type>text</type>
-    <cols>40</cols>
-    <rows>15</rows>
-XML;
+        $flexForm = '
+            <type>text</type>
+            <cols>40</cols>
+            <rows>15</rows>';
+
+        if (!$this->rte) {
+            return $flexForm;
+        }
+
+        $flexForm .= '<enableRichtext>1</enableRichtext>';
+
+        if (!$this->rteMode
+            || $this->isUsingLegacyRte()
+        ) {
+            return $flexForm;
+        }
+
+        if (isset($GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets'][$this->rteMode])) {
+            return $flexForm . "<richtextConfiguration>$this->rteMode</richtextConfiguration>";
+        }
+
+        /*
+         * If we get to here, the CKEditor preset was not found: we warn the
+         * user about it.
+         */
+        return "
+                <type>user</type>
+                <userFunc>CuyZ\Notiz\Notification\Service\NotificationTcaService->showCKEditorPresetMissing</userFunc>
+                <parameters>
+                    <preset>$this->rteMode</preset>
+                </parameters>";
+    }
+
+    /**
+     * @return string
+     */
+    public function getFlexFormAdditionalConfiguration()
+    {
+        $flexForm = '';
+
+        if ($this->rteMode
+            && $this->isUsingLegacyRte()
+        ) {
+            $flexForm .= "<defaultExtras>$this->rteMode</defaultExtras>";
+        }
+
+        return $flexForm;
+    }
+
+    /**
+     * @return bool
+     */
+    private function isUsingLegacyRte()
+    {
+        return ExtensionManagementUtility::isLoaded('rtehtmlarea')
+            && !ExtensionManagementUtility::isLoaded('rte_ckeditor');
     }
 }
