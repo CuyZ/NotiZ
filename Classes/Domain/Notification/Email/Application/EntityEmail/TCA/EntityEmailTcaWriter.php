6d9231b4877c0b5156e2bc039e42f865baf007a7
Romain Canon <romain.hydrocanon@gmail.com>
2018-09-05 20:17:26 +0200

    [FEATURE] Add description column to notification entities (#100)
    See: https://docs.typo3.org/typo3cms/TCAReference/Ctrl/Index.html#descriptioncolumn

--- Classes/Domain/Notification/Email/Application/EntityEmail/TCA/EntityEmailTcaWriter.php
+++ Classes/Domain/Notification/Email/Application/EntityEmail/TCA/EntityEmailTcaWriter.php
@@ -74,7 +74,7 @@ class EntityEmailTcaWriter extends EntityTcaWriter
                 '0' => [
                     'showitem' => '
                         error_message,
-                        title, sys_language_uid, hidden,
+                        title, description, sys_language_uid, hidden,
                         --div--;' . self::LLL_TABS . ':tab.event,
                             event, event_configuration_flex,
                         --div--;' . self::LLL_TABS . ':tab.channel,
