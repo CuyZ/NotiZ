e6083edd212f77e4a79bfb6f632a433c50cadb63
Romain Canon <romain.hydrocanon@gmail.com>
2018-09-13 00:49:26 +0200

    [FEATURE] Add asynchronous refresh to the notification toolbar
    A periodic asynchronous refresh has been added to the notification
    toolbar. These Ajax request will reload notification information every
    5 minutes in normal time, and every 30 seconds if an error occurred.
    
    Buttons have been added in the toolbar, allowing a manual refresh.
    
    One thing to note is that the first rendering of the toolbar (done
    during the TYPO3 backend rendering) does not contain information about
    existing notifications anymore (they will be fetched asynchronously).
    This can slightly improve performance when a lot of notifications were
    to be listed.

--- Classes/Backend/ToolBarItems/NotificationsToolbarItem.php
+++ Classes/Backend/ToolBarItems/NotificationsToolbarItem.php
@@ -17,13 +17,17 @@
 namespace CuyZ\Notiz\Backend\ToolBarItems;
 
 use CuyZ\Notiz\Core\Definition\DefinitionService;
+use CuyZ\Notiz\Service\Container;
 use CuyZ\Notiz\Service\ExtensionConfigurationService;
 use CuyZ\Notiz\Service\LocalizationService;
 use CuyZ\Notiz\Service\ViewService;
 use Exception;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Throwable;
 use TYPO3\CMS\Backend\Toolbar\ToolbarItemInterface;
 use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Page\PageRenderer;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\VersionNumberUtility;
 use TYPO3\CMS\Extbase\Object\ObjectManager;
@@ -34,6 +38,21 @@ use TYPO3\CMS\Fluid\View\StandaloneView;
  */
 class NotificationsToolbarItem implements ToolbarItemInterface
 {
+    /**
+     * If true, all information will be added to the toolbar menu.
+     *
+     * The first run of the toolbar rendering (during the rendering of the TYPO3
+     * backend) wont be full. This can improve performance if a lot of
+     * notifications were to be listed.
+     *
+     * Periodic asynchronous requests will be dispatched when the TYPO3 backend
+     * is rendered. These Ajax requests will contain all information, as they
+     * are running as background tasks.
+     *
+     * @var bool
+     */
+    protected $fullMenu = false;
+
     /**
      * @var IconFactory
      */
@@ -66,6 +85,8 @@ class NotificationsToolbarItem implements ToolbarItemInterface
         $this->definitionService = $objectManager->get(DefinitionService::class);
         $this->extensionConfigurationService = $objectManager->get(ExtensionConfigurationService::class);
         $this->viewService = $objectManager->get(ViewService::class);
+
+        $this->initializeJavaScript();
     }
 
     /**
@@ -97,36 +118,30 @@ class NotificationsToolbarItem implements ToolbarItemInterface
      */
     public function getDropDown()
     {
-        $exception = null;
-        $result = '';
-
         try {
-            $result = $this->getDropDownFromDefinition();
+            return $this->getDropDownFromDefinition();
         } catch (Throwable $exception) {
         } catch (Exception $exception) {
             // @PHP7
         }
 
-        if ($exception) {
-            $lllException = LocalizationService::localize('Backend/Toolbar/Error:exception');
-            $lllExceptionCode = LocalizationService::localize('Backend/Toolbar/Error:exception.code');
-            $lllExceptionMessage = LocalizationService::localize('Backend/Toolbar/Error:exception.message');
+        return $this->getErrorDropDown($exception);
+    }
 
-            $result = '<p class="text-danger">' . $lllException . '</p>
-                <hr />
-                <dl>';
+    /**
+     * Action called periodically by Ajax (used to refresh the toolbar).
+     *
+     * @param ServerRequestInterface $request
+     * @param ResponseInterface $response
+     * @return ResponseInterface
+     */
+    public function renderMenuAction(ServerRequestInterface $request, ResponseInterface $response)
+    {
+        $this->fullMenu = true;
 
-            if ($exception->getCode()) {
-                $result .= '<dt>' . $lllExceptionCode . '</dt>
-                    <dd>' . $exception->getCode() . '</dd>';
-            }
+        $response->getBody()->write($this->getDropDown());
 
-            $result .= '<dt>' . $lllExceptionMessage . '</dt>
-                <dd>' . $exception->getMessage() . '</dd>
-                </dl>';
-        }
-
-        return $result;
+        return $response->withHeader('Content-Type', 'text/html; charset=utf-8');
     }
 
     /**
@@ -138,22 +153,40 @@ class NotificationsToolbarItem implements ToolbarItemInterface
 
         if (!$this->definitionService->getValidationResult()->hasErrors()) {
             $definition = $this->definitionService->getDefinition();
-            $notifications = [];
-            $total = 0;
 
-            foreach ($definition->getNotifications() as $notification) {
-                $number = $notification->getProcessor()->getTotalNumber();
-                $total += $number;
-
-                if ($number > 0) {
-                    $notifications[] = $notification;
-                }
-            }
-
+            $view->assign('fullMenu', $this->fullMenu);
             $view->assign('definition', $definition);
-            $view->assign('filledNotifications', $notifications);
-            $view->assign('filledNotificationsTotal', $total);
+
+            if ($this->fullMenu) {
+                $notifications = [];
+                $total = 0;
+
+                foreach ($definition->getNotifications() as $notification) {
+                    $number = $notification->getProcessor()->getTotalNumber();
+                    $total += $number;
+
+                    if ($number > 0) {
+                        $notifications[] = $notification;
+                    }
+                }
+
+                $view->assign('filledNotifications', $notifications);
+                $view->assign('filledNotificationsTotal', $total);
+            }
         }
+
+        return $view->render();
+    }
+
+    /**
+     * @param Throwable $exception
+     * @return string
+     */
+    protected function getErrorDropDown($exception)
+    {
+        $view = $this->getFluidTemplateObject('Backend/ToolBar/NotificationToolBarDropDownError');
+        $view->assign('exception', $exception);
+        $view->assign('isAdmin', Container::getBackendUser()->isAdmin());
 
         return $view->render();
     }
@@ -177,6 +210,21 @@ class NotificationsToolbarItem implements ToolbarItemInterface
     }
 
     /**
+     * Requires the JavaScript script that will refresh the toolbar every now
+     * and then.
+     */
+    protected function initializeJavaScript()
+    {
+        $pageRenderer = $this->getPageRenderer();
+
+        $pageRenderer->loadRequireJsModule('TYPO3/CMS/Notiz/Toolbar');
+        $pageRenderer->addInlineLanguageLabelArray([
+            'notiz.toolbar.error.body' => LocalizationService::localize('Backend/Toolbar/Error:exception'),
+            'notiz.toolbar.error.refresh_label' => LocalizationService::localize('Backend/Toolbar/Show:refresh'),
+        ]);
+    }
+
+    /**
      * @param string $templateName
      * @return StandaloneView
      */
@@ -190,5 +238,13 @@ class NotificationsToolbarItem implements ToolbarItemInterface
         $view->assign('legacyLayout', $legacyLayout);
 
         return $view;
+    }
+
+    /**
+     * @return PageRenderer|object
+     */
+    protected function getPageRenderer()
+    {
+        return GeneralUtility::makeInstance(PageRenderer::class);
     }
 }
---
+++ Resources/Public/JavaScript/Toolbar.js
@@ -0,0 +1,274 @@
+define([
+    'jquery',
+    'TYPO3/CMS/Backend/Icons',
+    'TYPO3/CMS/Backend/Notification',
+    'TYPO3/CMS/Backend/Storage'
+], function ($, Icons, Notification, Storage) {
+    'use strict';
+
+    var selector = {
+        toolbarContainer: '#cuyz-notiz-backend-toolbaritems-notificationstoolbaritem',
+        menuContainer: '.dropdown-menu',
+        toolbarIcon: '.toolbar-item-icon .t3js-icon',
+        /**
+         * @deprecated Must be removed when TYPO3 v7 is not supported anymore.
+         */
+        legacyToolbarIcon: '.dropdown-toggle span.icon',
+        dataContainer: '.t3js-notiz-data-container',
+        iconContainer: '.t3js-notiz-icon'
+    };
+
+    var menu = (function () {
+        var toolbarContainer = $(selector.toolbarContainer),
+            menuContainer = toolbarContainer.find(selector.menuContainer);
+
+        return {
+            container: menuContainer,
+
+            content: function (content) {
+                menuContainer.html(content);
+            },
+            
+            show: function () {
+                toolbarContainer.addClass('open');
+            },
+
+            /**
+             * Updates the content of the toolbar menu: it is fetched with an
+             * Ajax request.
+             */
+            update: function (callback) {
+                timer.reset();
+
+                loading.run(function () {
+                    $.ajax({
+                        url: TYPO3.settings.ajaxUrls['notiz_render_toolbar'],
+                        data: {
+                            skipSessionUpdate: 1
+                        },
+                        type: 'post',
+                        cache: false,
+                        success: function (result) {
+                            // Display the new content of the toolbar.
+                            menu.content(result);
+
+                            // Update the icon (can have an error overlay).
+                            icon.update(menu.container.find(selector.iconContainer).html());
+
+                            // Send a flash message to the user.
+                            var data = menu.data();
+                            message.new(data.type).send(data.message.title, data.message.body);
+
+                            if (data.error) {
+                                timer.fastTick();
+                            } else {
+                                timer.defaultTick();
+                            }
+                        },
+                        error: menu.error,
+                        complete: function () {
+                            timer.launch();
+
+                            if (typeof callback !== 'undefined') {
+                                callback();
+                            }
+                        }
+                    });
+                });
+            },
+
+            /**
+             * Fetches data from a hidden element in the menu container. These
+             * data are updated every time the toolbar is refreshed.
+             */
+            data: function () {
+                var container = menuContainer.find(selector.dataContainer);
+
+                return {
+                    error: !!container.data('error'),
+                    type: container.data('error') ? 'error' : 'success',
+                    message: {
+                        title: container.data('message-title'),
+                        body: container.data('message-body')
+                    }
+                }
+            },
+
+            /**
+             * If something went wrong when refreshing the toolbar, an error
+             * message is displayed.
+             */
+            error: function () {
+                timer.fastTick();
+
+                // Reset the icon with its initial value.
+                icon.update();
+
+                // Display some error messages and a refresh button.
+                var refreshLabel = TYPO3.lang['notiz.toolbar.error.refresh_label'];
+
+                menu.content(
+                    '<p class="dropdown-text text-danger">' + TYPO3.lang['notiz.toolbar.error.body'] + '</p>' +
+                    '<hr />' +
+                    '<p class="dropdown-text">' +
+                    '<a href="#" onclick="TYPO3.NotificationToolbar.menu.refresh();" title="' + refreshLabel + '">' +
+                    '<span class="refresh-icon"></span>' + '&nbsp;' + refreshLabel +
+                    '</a>' +
+                    '</p>'
+                );
+
+                // Urgh.
+                Icons.getIcon('actions-refresh', Icons.sizes.small, '', '', 'inline')
+                    .done(function (refreshIcon) {
+                        menu.container.find('.refresh-icon').replaceWith(refreshIcon);
+                    });
+            }
+        };
+    })();
+
+    var loading = {
+        run: function (callback) {
+            Icons.getIcon('spinner-circle-light', Icons.sizes.small).done(function (spinner) {
+                menu.content('<p class="text-center">' + spinner + '</p>');
+                icon.update(spinner);
+
+                callback();
+            });
+        }
+    };
+
+    var icon = (function () {
+        var container = function () {
+            var element = $(selector.toolbarIcon, selector.toolbarContainer);
+
+            if (element.length === 0) {
+                element = $(selector.legacyToolbarIcon, selector.toolbarContainer);
+            }
+
+            return element;
+        };
+
+        /**
+         * Initial icon of the toolbar. We keep it in memory in case something
+         * goes wrong, in which case it will be re-used.
+         */
+        var backupIcon = container().html();
+
+        return {
+            update: function (newIcon) {
+                container().html(newIcon || backupIcon);
+            }
+        }
+    })();
+
+    /**
+     * Timer used for refreshing the toolbar.
+     */
+    var timer = (function () {
+        var instance = null,
+            defaultTick = 1000 * 60 * 5, // 5 minutes
+            tick = defaultTick;
+
+        return {
+            reset: function () {
+                if (instance !== null) {
+                    clearTimeout(instance);
+                    instance = null;
+                }
+            },
+            launch: function () {
+                instance = setTimeout(menu.update, tick);
+            },
+            tick: function (newTick) {
+                tick = 1000 * newTick
+            },
+            defaultTick: function () {
+                tick = defaultTick;
+            },
+            /**
+             * Speeds up the tick of the timer to increase chance to get a valid
+             * toolbar faster.
+             */
+            fastTick: function () {
+                tick = 1000 * 30; // 30 seconds
+            }
+        }
+    })();
+
+    var message = (function() {
+        var typeKey = 'notiz.toolbar.message_type';
+
+        return {
+            new: function (type) {
+                return {
+                    /**
+                     * Displays a flash message for the user, telling him/her if
+                     * notifications are enabled or disabled.
+                     */
+                    send: function (title, body) {
+                        if (!title && !body) {
+                            return;
+                        }
+
+                        var lastType = Storage.Persistent.isset(typeKey)
+                            ? Storage.Persistent.get(typeKey)
+                            : null;
+
+                        /*
+                         * We display the message only if it was not displayed
+                         * previously.
+                         */
+                        if (lastType === type) {
+                            return;
+                        }
+                        
+                        if (type === 'error') {
+                            Notification.error(title, body);
+                        } else {
+                            Notification.success(title, body, 10);
+                        }
+
+                        Storage.Persistent.set(typeKey, type);
+                    }
+                }
+            }
+        }
+    })();
+
+    try {
+        /**
+         * Registers the menu update as an event for the whole TYPO3 toolbar.
+         */
+        TYPO3.Backend.Topbar.Toolbar.registerEvent(menu.update);
+    } catch (e) {
+        /**
+         * @deprecated Must be removed when TYPO3 v7 is not supported anymore.
+         *
+         * Steps to follow:
+         * - Add `TYPO3/CMS/Backend/Viewport` as dependency in the RequireJS
+         *   section of this file.
+         * - Use the following statement instead of this try/catch block:
+         *   `Viewport.Topbar.Toolbar.registerEvent(menu.update);`
+         */
+        $(menu.update);
+    }
+
+    /**
+     * Public API of this module.
+     */
+    return TYPO3.NotificationToolbar = {
+        menu: {
+            update: menu.update,
+
+            /**
+             * Same as update, but at the end of the process the toolbar menu is
+             * displayed.
+             */
+            refresh: function () {
+                menu.update(function () {
+                    menu.show();
+                });
+            }
+        }
+    };
+});
