4c76bc00408dca0af4c29c9b6e2dc635856a3389
Romain Canon <romain.hydrocanon@gmail.com>
2018-09-19 17:56:27 +0200

    [TASK] Use correct links to administration module

--- Classes/Controller/Backend/Administration/ShowDefinitionController.php
+++ Classes/Controller/Backend/Administration/ShowDefinitionController.php
@@ -55,7 +55,7 @@ class ShowDefinitionController extends BackendController
         $exception = $this->runtimeService->getException();
 
         if (!$exception) {
-            $this->forward('showDefinition');
+            $this->forward('process');
         }
 
         throw $exception;
