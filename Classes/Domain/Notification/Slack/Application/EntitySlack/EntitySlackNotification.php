8e746ea084f943788121247eed68905ba33262b0
Nathan Boiron <nathan.boiron@gmail.com>
2018-09-25 20:26:32 +0200

    [TASK] Add a way to mark notifications as creatable by an editor (#122)

--- Classes/Domain/Notification/Slack/Application/EntitySlack/EntitySlackNotification.php
+++ Classes/Domain/Notification/Slack/Application/EntitySlack/EntitySlackNotification.php
@@ -16,13 +16,14 @@
 
 namespace CuyZ\Notiz\Domain\Notification\Slack\Application\EntitySlack;
 
+use CuyZ\Notiz\Core\Notification\Creatable;
 use CuyZ\Notiz\Core\Notification\CustomSettingsNotification;
 use CuyZ\Notiz\Domain\Notification\EntityNotification;
 use CuyZ\Notiz\Domain\Notification\Slack\Application\EntitySlack\Processor\EntitySlackNotificationProcessor;
 use CuyZ\Notiz\Domain\Notification\Slack\Application\EntitySlack\Settings\EntitySlackSettings;
 use CuyZ\Notiz\Domain\Notification\Slack\SlackNotification;
 
-class EntitySlackNotification extends EntityNotification implements SlackNotification, CustomSettingsNotification
+class EntitySlackNotification extends EntityNotification implements SlackNotification, CustomSettingsNotification, Creatable
 {
     /**
      * @var string
