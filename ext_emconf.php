01c3cfba3b03e0e2ba6c2b71faf6287f02d5547e
Lukas Niestroj <niestrojlukas@gmail.com>
2021-06-29 10:12:35 +0200

    [TASK] raise major version; only for TYPO3 10

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,19 +4,19 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'NotiZ • Powerful notification dispatcher',
     'description' => 'Handle any type of notification in TYPO3 with ease: emails, SMS, Slack and more. Listen to your own events or provided ones (scheduler task finishing, extension installed, etc…).',
 
-    'version' => '2.1.2',
+    'version' => '3.0.0',
     'state' => 'stable',
 
     'author' => 'Romain Canon, Nathan Boiron',
     'author_email' => 'team.cuyz@gmail.com',
 
     'category' => 'be',
-    'clearCacheOnLoad' => 1,
+    'clearCacheOnLoad' => true,
 
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.5.99',
-            'configuration_object' => '1.10.0-2.99.99',
+            'typo3' => '10.4.0-10.4.99',
+            'configuration_object' => '3.0.0-3.9.99',
         ],
     ],
 ];
