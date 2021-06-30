032806b686948c6375dcc20d08ab43e2abbb978e
Lukas Niestroj <niestrojlukas@gmail.com>
2021-06-30 11:15:31 +0200

    [TASK] re-register Events in middleware

---
+++ Classes/Middleware/EventDefinitionRegistererMiddleware.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * Copyright (C)
+ * Nathan Boiron <nathan.boiron@gmail.com>
+ * Romain Canon <romain.hydrocanon@gmail.com>
+ *
+ * This file is part of the TYPO3 NotiZ project.
+ * It is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License, either
+ * version 3 of the License, or any later version.
+ *
+ * For the full copyright and license information, see:
+ * http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace CuyZ\Notiz\Middleware;
+
+use CuyZ\Notiz\Core\Event\Service\EventRegistry;
+use CuyZ\Notiz\Service\RuntimeService;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Throwable;
+
+class EventDefinitionRegistererMiddleware implements MiddlewareInterface
+{
+    /**
+     * @param ServerRequestInterface $request
+     * @param RequestHandlerInterface $handler
+     * @return ResponseInterface
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        try {
+            EventRegistry::get()->registerEvents();
+        } catch (Throwable $exception) {
+            RuntimeService::get()->setException($exception);
+        }
+
+        return $handler->handle($request);
+    }
+}
